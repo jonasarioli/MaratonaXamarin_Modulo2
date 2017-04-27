6fad9c543f36ed2f40942c81b4880e571ce386ce
Jonas Arioli <jarioli@lenovo.com>
2017-04-27 17:30:56 -0300

    Add project files.

---
+++ MaratonaXamarin_Modulo2/MaratonaXamarin_Modulo2/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaratonaXamarin_Modulo2.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _propriedadeTexto = "Texto Inicial";
+
+        public string PropriedadeTexto
+        {
+            get { return _propriedadeTexto; }
+            set
+            {
+                _propriedadeTexto = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
